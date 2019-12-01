1050925ec0a1acc100460c82a35212526954eaf0
Ryno Claassen <rynoclaassen@gmail.com>
2019-12-01 17:20:50 +0200

    Initial commit

---
+++ Volume Calculator/Volume Calculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Volume Calculator
+//
+//  Created by Ryno Claassen on 2019/12/01.
+//  Copyright © 2019 Ryno Claassen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
